246e89d562b660cac99525cd5bbc60063a58a013
vincecloutier <vincentcloutier@mail.com>
2022-08-03 16:10:24 -0400

    added dataset

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ def nonlin(x, deriv=False):
     if deriv:
         return x*(1-x)
     return 1/(1+np.exp(-x))
+
+#input dataset
+X = np.array([
+                [0,0,1],
+                [0,1,1],
+                [1,0,1],
+                [1,1,1]
+            ])
