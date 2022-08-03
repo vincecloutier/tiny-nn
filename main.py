85cfa70a275a1f59a729af505f0139558dc4d628
vincecloutier <vincentcloutier@mail.com>
2022-08-03 16:35:25 -0400

    still an error

--- main.py
+++ main.py
@@ -20,20 +20,28 @@ syn0 = 2*np.random.random((3,4)) - 1
 syn1 = 2*np.random.random((4,1)) - 1
 
 
-for iter in range(10000):
+for j in range(60000):
 
     # forward propagation
-    l0 = X # first layer, specified by the input data
-    l1 = nonlin(np.dot(l0, syn0)) # second or hidden layer
+    l0 = X
+    l1 = nonlin(np.dot(l0, syn0))
+    l2 = nonlin(np.dot(l1, syn1))
 
-    # how much did we miss?
-    l1_error = y - l1
+    # check error
+    l2_error = y - l2
+    if (j% 10000) == 0:
+        print("Error:" + str(np.mean(np.abs(l2_error))))
+    # adjust based on certainty
+    l2_delta = l2_error*nonlin(l2,deriv=True)
 
-    # multiply how much we missed by the slope of the sigmoid at the values in layer 1 (l1)
+    # check how much l1 value contributed to l2 error
+    l1_error = l2_delta.dot(syn1.T)
+    # adjust based on certainty
     l1_delta = l1_error * nonlin(l1, True)
 
     # update weights
-    syn0 += np.dot(l0.T, l1_delta)
+    syn1 += l1.T.dot(l2_delta)
+    syn0 += l0.T.dot(l1_delta)
 
 print("Output After Training:")
 print(l1)
