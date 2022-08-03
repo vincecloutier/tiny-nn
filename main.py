ace349a0d935767cd6f1a1371e28ccaa566f9f24
vincecloutier <vincentcloutier@mail.com>
2022-08-03 16:11:48 -0400

    addded output data + seeding random numbers

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ def nonlin(x, deriv=False):
         return x*(1-x)
     return 1/(1+np.exp(-x))
 
-#input dataset
-X = np.array([
-                [0,0,1],
-                [0,1,1],
-                [1,0,1],
-                [1,1,1]
-            ])
+#input data
+X = np.array([[0,0,1], [0,1,1], [1,0,1], [1,1,1]])
+
+#output data
+y = np.array([[0,0,0,1]]).T
+
+# seed random numbers to make calculation deterministic (just a good practice)
+np.random.seed(1)
