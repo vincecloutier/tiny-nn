15c38e3cd517b49ed8a0844ff619af46fc3b6f9e
vincecloutier <vincentcloutier@mail.com>
2022-08-03 16:49:52 -0400

    accurate predictions & error rates

--- main.py
+++ main.py
@@ -5,27 +5,25 @@ def nonlin(x,deriv=False):
     if(deriv==True):
         return x*(1-x)
     return 1/(1+np.exp(-x))
-
+ 
 # input data
 X = np.array([[0,0,1], [0,1,1], [1,0,1], [1,1,1]])
-
-# output data
-y = np.array([[0,1,1,0]])
+#output data
+y = np.array([[0],[1],[1],[0]])
 
 # seed random numbers to make calculation deterministic (just a good practice)
 np.random.seed(1)
 
-# randomly initialize weights with mean 0
+# randomly initialize our weights with mean 0
 syn0 = 2*np.random.random((3,4)) - 1
 syn1 = 2*np.random.random((4,1)) - 1
-
 
 for j in range(60000):
 
     # forward propagation
     l0 = X
-    l1 = nonlin(np.dot(l0, syn0))
-    l2 = nonlin(np.dot(l1, syn1))
+    l1 = nonlin(np.dot(l0,syn0))
+    l2 = nonlin(np.dot(l1,syn1))
 
     # check error
     l2_error = y - l2
@@ -37,11 +35,11 @@ for j in range(60000):
     # check how much l1 value contributed to l2 error
     l1_error = l2_delta.dot(syn1.T)
     # adjust based on certainty
-    l1_delta = l1_error * nonlin(l1, True)
+    l1_delta = l1_error * nonlin(l1,deriv=True)
 
     # update weights
     syn1 += l1.T.dot(l2_delta)
     syn0 += l0.T.dot(l1_delta)
 
 print("Output After Training:")
-print(l1)
+print(l2)
