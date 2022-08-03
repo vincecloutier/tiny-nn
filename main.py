fd78b0c4fb8daa910bcfe6b6f56559502236b845
vincecloutier <vincentcloutier@mail.com>
2022-08-03 16:12:47 -0400

    initalizing weights

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import numpy as np
 
-
-#sigmoid function
+# sigmoid function
 def nonlin(x, deriv=False):
     if deriv:
         return x*(1-x)
     return 1/(1+np.exp(-x))
 
-#input data
+# input data
 X = np.array([[0,0,1], [0,1,1], [1,0,1], [1,1,1]])
 
-#output data
+# output data
 y = np.array([[0,0,0,1]]).T
 
 # seed random numbers to make calculation deterministic (just a good practice)
 np.random.seed(1)
+
+# initialize weights randomly with mean 0
+syn0 = 2*np.random.random((3,1)) - 1
